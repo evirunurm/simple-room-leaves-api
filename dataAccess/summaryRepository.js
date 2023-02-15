c4b87c0313fbf80c3b85f8815aa95fbddc418ff6
Evelin <teakid21@gmail.com>
2023-02-15 14:53:18 +0100

    fixed a typo

--- dataAccess/summaryRepository.js
+++ dataAccess/summaryRepository.js
@@ -6,13 +6,13 @@ const jsonFile = path.join(process.cwd(), 'dataAccess/data', 'summaryData.json')
 const getLatest = async (days) => {
     const parsed = JSON.parse(fs.readFileSync(jsonFile)).days.slice(0, days);
 
-	return parsed.days;
+	return parsed;
 }
 
 const findAll = async () => {
     const parsed = JSON.parse(fs.readFileSync(jsonFile)).days.slice(0, 10);
 
-	return parsed.days;
+	return parsed;
 }
 
 
